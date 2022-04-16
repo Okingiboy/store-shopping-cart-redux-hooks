7ce6d8d25d215efcbdc212da9de2ab72086b9386
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-04-16 11:56:40 -0500

    updated files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/salty-spire-13469', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/salty-spire-13469', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
